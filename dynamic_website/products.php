02fc3e12f183235a4772066f27eed28c1cf93134
Anish <bhalla04@myunitec.ac.nz>
2020-06-16 16:55:14 +1200

    product

--- dynamic_website/products.php
+++ dynamic_website/products.php
@@ -4,11 +4,43 @@
 include_once "header.php";
 ?>
 
+<div id = "catDivContainer">
+    <div id = 'catDiv'>
+<?php
+    $categoryID = $_GET["categoryID"];
+    $products = $user->showProductsByCategory($categoryID);
+    $i = 0;
+    while ($i<sizeof($products)){
+        $product = $products[$i];
+?>
+<
+        <ul>
+            <li>
+                <img src="<?php echo $product->image;?>"/>
+                <p>"<?php echo $product->name; ?>"</p>
+                <p> = "Price: $<?php echo $product->price; ?>"</p>
 
+                Qty: <input type = number id = 'dogFood1Qty' class = qty>
+
+                <button name="<?php echo $product->name; ?>"
+                        price = "<?php echo $product->price; ?>"
+                        onclick="addProductToCart(this)">Add to cart</button>
+
+            </li>
+        </ul>
+
+    </div>
+</div>
+
+<?php
+$i = $i + 1;
+}
+?>
 
 <?php
 include_once "categories.php";
 ?>
+
 
 <?php
 include_once "footer.php";
