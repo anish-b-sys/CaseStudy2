3ea1619333f4a048dda647e4f304fb6f30567bd2
Anish <bhalla04@myunitec.ac.nz>
2020-06-16 17:05:52 +1200

    productfix

--- dynamic_website/products.php
+++ dynamic_website/products.php
@@ -1,4 +1,17 @@
-<link rel="stylesheet" href="css/styles.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>products</title>
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body onload="invoice()">
 
 <?php
 include_once "header.php";
@@ -13,7 +26,6 @@ include_once "header.php";
     while ($i<sizeof($products)){
         $product = $products[$i];
 ?>
-<
         <ul>
             <li>
                 <img src="<?php echo $product->image;?>"/>
@@ -28,14 +40,14 @@ include_once "header.php";
 
             </li>
         </ul>
-
-    </div>
-</div>
-
 <?php
 $i = $i + 1;
 }
 ?>
+    </div>
+</div>
+
+
 
 <?php
 include_once "categories.php";
@@ -45,3 +57,6 @@ include_once "categories.php";
 <?php
 include_once "footer.php";
 ?>
+
+</body>
+</html>
