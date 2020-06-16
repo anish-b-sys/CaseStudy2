1152ed5cc0769ffbb23830d6667ac216c1687f12
Anish <bhalla04@myunitec.ac.nz>
2020-06-16 13:37:23 +1200

    Fix Img

--- dynamic_website/products.php
+++ dynamic_website/products.php
@@ -7,7 +7,7 @@
         $i = 0;
         while ($i < sizeof($categories)){
             $category = $categories[$i];
-            echo "<li><a href='products.php?categoryID=".$category->id."'><img src =.$category->picture./></a></li>";
+            echo "<li><a href='products.php?categoryID=".$category->id."'><img src =".$category->picture."/></a></li>";
             echo $category->picture;
             $i = $i + 1;
         }
